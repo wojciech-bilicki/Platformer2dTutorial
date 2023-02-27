35aed2b473ed9be7727aa4aa310c328b959ec1e4
wojciech-bilicki <wojciech.bilicki@gmail.com>
2023-02-27 23:45:16 +0100

    Add weighted platform part 10

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,41 +6,44 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
+    public float distanceToGround = 0;
+    
     [SerializeField] private float speed = 5;
     [SerializeField] private LayerMask groundLayerMask;
 
     private Rigidbody2D _rigidbody2D;
-    private BoxCollider2D _boxCollider2D;
+    private PolygonCollider2D _polygonCollider2D;
     private Vector2 _velocity;
 
-    private float _heightCheckDisctance = .1f;
+    private float _heightCheckDisctance = .015f;
     private InputManager _inputManager;
     private AnimationController _animationController;
     private Transform _spriteTransform;
+    
 
     //TODO: this could be represented as enum 
     #region PlayerStates
-    private bool _isGrounded;
+    public bool _isGrounded;
     private bool _isJumping;
     #endregion
 
     #region JumpingProperties
 
-    [SerializeField] private float fallingDownMultiplier =2.0f;
+    [SerializeField] public float fallingDownMultiplier =2.0f;
     [SerializeField] private float maxJumpHeight = 2.5f;
     [SerializeField] private float maxJumpTime = 1.0f;
 
     private float jumpForce =>  ( maxJumpHeight) / (maxJumpTime / 2f);
     private float gravity => (-2f * maxJumpHeight) / Mathf.Pow(maxJumpTime / 2f, 2);
-    
+
+    public bool disableGravity = false;
     #endregion
     
     // Start is called before the first frame update
     void Awake()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
-        _boxCollider2D = GetComponent<BoxCollider2D>();
+        _polygonCollider2D = GetComponent<PolygonCollider2D>();
         _inputManager = GetComponent<InputManager>();
         _animationController = GetComponent<AnimationController>();
         _spriteTransform = GetComponentInChildren<Transform>();
@@ -56,7 +59,11 @@ public class PlayerMovement : MonoBehaviour
             TryApplyJump();
         }
 
-        ApplyGravity();
+        if (!disableGravity)
+        {
+              ApplyGravity();
+        }
+      
         _animationController.SetIsWalking(_isGrounded && _velocity.x != 0);
         _animationController.SetIsJumping(_isJumping);
 
@@ -66,7 +73,7 @@ public class PlayerMovement : MonoBehaviour
     private void ApplyGravity()
     {
         bool isFalling = _velocity.y < 0.0f;
-        float gravityMultiplier = isFalling  ? fallingDownMultiplier : 1f;
+        float gravityMultiplier = isFalling  ? fallingDownMultiplier : 1;
         float jumpForceMultiplier = _inputManager.IsJumpingPressed ? 2f : 1f;
         _velocity.y += gravity * Time.deltaTime * gravityMultiplier / jumpForceMultiplier;
         _velocity.y = Mathf.Max(_velocity.y, gravity / 2f);
@@ -105,9 +112,9 @@ public class PlayerMovement : MonoBehaviour
     bool IsGrounded()
     {
       
-        Vector3 raycastOrigin = _boxCollider2D.bounds.center;
+        Vector3 raycastOrigin = _polygonCollider2D.bounds.center;
         
-        RaycastHit2D raycastHit2D = Physics2D.BoxCast(raycastOrigin, _boxCollider2D.bounds.size, 0, Vector2.down,
+        RaycastHit2D raycastHit2D = Physics2D.BoxCast(raycastOrigin, _polygonCollider2D.bounds.size, 0, Vector2.down,
             _heightCheckDisctance, groundLayerMask);
 
         Color rayColor;
@@ -120,6 +127,8 @@ public class PlayerMovement : MonoBehaviour
         {
             rayColor = Color.red;
         }
+
+        distanceToGround = raycastHit2D.distance;
         
         Debug.DrawRay(raycastOrigin, Vector3.down *(_heightCheckDisctance), rayColor, groundLayerMask);
         return raycastHit2D.collider != null;
@@ -129,7 +138,7 @@ public class PlayerMovement : MonoBehaviour
     public void OnPlayerDeath()
     {
         _inputManager.enabled = false;
-        _boxCollider2D.enabled = false;
+        _polygonCollider2D.enabled = false;
         _velocity.y = jumpForce * 2;
         _rigidbody2D.constraints = RigidbodyConstraints2D.None;
         _rigidbody2D.AddTorque(20, ForceMode2D.Impulse);
--- Assets/Scripts/WeightedPlatform.cs
+++ Assets/Scripts/WeightedPlatform.cs
@@ -6,67 +6,96 @@ public class WeightedPlatform : MonoBehaviour
 {
     private bool shouldPlatformRotate;
     private Transform player;
+    private PlayerMovement _playerMovement;
     
     private float _platformLength;
+    private float _heightCheckDistance = 0.5f;
+    private BoxCollider2D _platformBoxCollider;
 
+    [SerializeField] private LayerMask playerLayerMask;
     [SerializeField] private float rotationMaxDegrees = 30;
     // [SerializeField] private BoxCollider2D _playerLeavePlatformCollider;
     private void Awake()
     {
-        _platformLength = GetComponent<BoxCollider2D>().bounds.size.x;
-        Debug.Log(_platformLength);
-       
+        _platformBoxCollider = GetComponent<BoxCollider2D>();
+        _platformLength = _platformBoxCollider.bounds.size.x;
+
     }
 
-    private void OnTriggerEnter2D(Collider2D col)
+    private void OnCollisionEnter2D(Collision2D col)
     {
         if (col.gameObject.CompareTag("Player"))
         {
             shouldPlatformRotate = true;
             //get player transform
             player = col.gameObject.GetComponent<Transform>();
-            col.gameObject.GetComponent<Rigidbody2D>().freezeRotation = false;
-        }
-    }
+            _playerMovement = col.gameObject.GetComponent<PlayerMovement>();
 
-    private void OnTriggerExit2D(Collider2D col)
-    {
-        if (col.gameObject.CompareTag("Player"))
-        {
-            shouldPlatformRotate = false;
-            player = null;
-            col.gameObject.GetComponent<Transform>().rotation = Quaternion.Euler(0,0,0);
-            col.gameObject.GetComponent<Rigidbody2D>().freezeRotation = true;
         }
     }
 
     private void Update()
     {
-        // Debug.Log(GetDistanceBetweenPlayerAndTransform());
         if (shouldPlatformRotate)
-        {   
+        {
+            RaycastHit2D raycastHit2D = Physics2D.BoxCast(_platformBoxCollider.bounds.center, _platformBoxCollider.bounds.size, 0, Vector2.up,
+                _heightCheckDistance, playerLayerMask);
+            if (raycastHit2D.collider == null)
+            { 
+                player = null;
+                shouldPlatformRotate = false;
+                _playerMovement = null;
+                return;
+            }
+            
             Vector3 playerRelativePosition = transform.InverseTransformPoint(player.position);
             int rotationDirection = playerRelativePosition.x < 0 ? 1 : -1;
             float rotationSpeedMultiplier = Mathf.Abs(Mathf.Clamp((playerRelativePosition.x * 2 / _platformLength) * rotationDirection, -1, 1));
-            
-            if (transform.rotation.eulerAngles.z< rotationMaxDegrees || transform.rotation.eulerAngles.z  > 360 - rotationMaxDegrees)
+
+            bool canPlatformRotate = CanPlatformRotate(rotationDirection);
+            if( canPlatformRotate)
+
             {
                 transform.rotation = Quaternion.RotateTowards(transform.rotation,
                     Quaternion.AngleAxis(rotationMaxDegrees * rotationDirection, Vector3.forward),
-                    Time.deltaTime  * 50 * rotationSpeedMultiplier);
-             }
+                    Time.deltaTime * 20 * rotationSpeedMultiplier);
+
+                if (_playerMovement && _playerMovement.distanceToGround > 0)
+                {
+                    Debug.Log(_playerMovement.distanceToGround);
+                    player.GetComponent<Rigidbody2D>().MovePosition(new Vector2(player.transform.position.x,
+                        player.transform.position.y - _playerMovement.distanceToGround));
+                }
+
+            }
+            
         } else if (transform.rotation.eulerAngles.z != 0)
         {
             transform.rotation = Quaternion.RotateTowards(transform.rotation,
                 Quaternion.Euler(0, 0, 0),
                 Time.deltaTime  * 50);
+
         } 
         
     }
 
 
-    public float GetDistanceBetweenPlayerAndTransform()
+    public bool CanPlatformRotate(int rotationDirection)
     {
-        return transform.position.y - player.position.y;
+        bool canRotate = false;
+        if ((transform.rotation.eulerAngles.z < rotationMaxDegrees)
+            || (transform.rotation.eulerAngles.z > 360 - rotationMaxDegrees))
+        {
+            canRotate = true;
+        } else if (transform.rotation.eulerAngles.z >= rotationMaxDegrees && rotationDirection == 1)
+        {
+            canRotate = true;
+        } else if (transform.rotation.eulerAngles.z <= 360 - rotationMaxDegrees && rotationDirection == -1)
+        {
+            canRotate = true;
+        }
+        
+
+        return canRotate;
     }
 }
